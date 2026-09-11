69a2747a9598fc06ded0d7454f663a20f93765a7
Igor <igordossantoslopes73@gmail.com>
2026-09-11 19:09:45 -0300

    voltando para o nome function_app.py, foi decidido que vamos manter as 4 functions em um só arquivo.

--- function_app.py
+++ function_app.py
@@ -3,10 +3,15 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
+# FUNCTION 1
+# timertrigger que impri um log
 @app.timer_trigger(schedule="*/2 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def timer_trigger_log(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('Imprimiu atrasado.')
 
-    logging.info('Imprimi log.')
+    logging.info('Log imprimido.')
+
+
+# FUNCTION 2 
